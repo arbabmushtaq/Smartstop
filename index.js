0c4836f29db7e1454ee7a818d635ea7e393e97bc
Arbab <arbabmushtaq96@gmail.com>
2017-11-12 05:01:56 +0500

    Server Setup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const  bodyParser = require('body-parser')
 const  app        =  express();
 const  mongoose   = require('mongoose');
 const  router     = express.Router();
-// const  cors       = require('cors'); //Just for Development mode, client and server interaction
 
 const  config     = require('./config/database');
 const  path       = require('path') ;
@@ -21,27 +20,6 @@ mongoose.connect( config.uri , (err)=>{
         console.log('connected to database : '+config.db);
     }
 });
-
-// //middleware for cors, Just for development mode
-// app.use(cors({
-//     origin: 'http://localhost:4200'
-// }));
-//
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
-// // parse application/json
-// app.use(bodyParser.json());
-//
-// //middleware static directory for frontend
-// app.use(express.static(__dirname+'/client/dist/'));
-//
-// //our custom authentication middleware ,API
-// app.use('/authentication' , authentication );
-//
-// app.get('*',(req , res )=>{
-//         res.sendFile(path.join(__dirname +'/client/dist/index.html'))
-// });
-
 //serverlisting
 var port =  process.env.PORT || 1212;
 app.listen( port , () => {
